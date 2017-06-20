020d70d4f60fe289ed76608f13618238f4c54c96
Dominik <dominikb17106@gmail.com>
2017-06-20 19:39:51 +0200

    Początek konfiguracji maven

---
+++ isite-App/src/main/java/pl/isite/IsiteApplication.java
@@ -0,0 +1,12 @@
+package pl.isite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsiteApplication.class, args);
+	}
+}
